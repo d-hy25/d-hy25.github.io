62719274d39c0a99b574c3c14b2a351998244adb
d-hy25 <rnjsehgus2525@gmail.com>
2025-02-20 17:58:50 +0900

    update

--- OsullocWebpublishing/script/clock.js
+++ OsullocWebpublishing/script/clock.js
@@ -20,3 +20,33 @@ const getClock =()=>{
 }
 setInterval(getClock, 1000);
 getClock();
+// 하루 남은 시간 계산
+// // 현재 날짜와 시간을 가져옵니다.
+// let currentDate = new Date();
+
+// // 현재 시, 분, 초를 가져옵니다.
+// let currentHours = currentDate.getHours();
+// let currentMinutes = currentDate.getMinutes();
+// let currentSeconds = currentDate.getSeconds();
+
+// // 하루의 총 초 (24시간)
+// const totalSecondsInDay = 24 * 60 * 60; // 86400초
+
+// // 자정으로부터 현재까지 경과한 시간을 초로 계산합니다.
+// let currentTimeInSeconds = (currentHours * 3600) + (currentMinutes * 60) + currentSeconds;
+
+// // 남은 시간을 초로 계산합니다.
+// let remainingTimeInSeconds = totalSecondsInDay - currentTimeInSeconds;
+
+// // 남은 초를 시, 분, 초로 변환합니다.
+// let remainingHours = Math.floor(remainingTimeInSeconds / 3600);
+// let remainingMinutes = Math.floor((remainingTimeInSeconds % 3600) / 60);
+// let remainingSeconds = remainingTimeInSeconds % 60;
+
+// // 한 자리 수일 경우 앞에 0을 추가하여 포맷을 맞춥니다.
+// remainingHours = remainingHours.toString().padStart(2, '0');
+// remainingMinutes = remainingMinutes.toString().padStart(2, '0');
+// remainingSeconds = remainingSeconds.toString().padStart(2, '0');
+
+// // 결과를 출력합니다.
+// console.log(`남은 시간: ${remainingHours}시간 ${remainingMinutes}분 ${remainingSeconds}초`);
--- Portfolio/javascript/mouseMove.js
+++ Portfolio/javascript/mouseMove.js
@@ -4,6 +4,8 @@ document.addEventListener("mousemove",(event)=>{
   const $box = document.querySelector(".box");
   $box.style.left = `${event.clientX}px`;
   $box.style.top = `${event.clientY}px`;
+  //// 커스텀 커서가 마우스 이벤트를 방해하지 않도록 설정
+  $box.style.pointerEvents = "none";
 });
 document.addEventListener('mouseup', () => {
   const $box = document.querySelector(".box");
@@ -26,19 +28,19 @@ document.addEventListener("contextmenu",(event)=>{
   //***마우스 오른쪽 눌렀을때 브라우저 안뜨게 하기***/
   event.preventDefault();
 
+// //   const $elem = document.querySelector(".coord");
+// //   // $elem.textContent = "마우스 오른쪽 버튼 클릭!!!";
+// //   $elem.style.left = `${event.clientX}px`;
+// //   $elem.style.top = `${event.clientY}px`;
+// });
+// //마우스 업 이벤트 처리
+// document.addEventListener("mouseup",(event)=>{
+//   // console.log(event);
+//   if(event.button === 2){ //오른쪽 버튼
+//     console.log("오른쪽 버튼 업");
 //   const $elem = document.querySelector(".coord");
-//   // $elem.textContent = "마우스 오른쪽 버튼 클릭!!!";
-//   $elem.style.left = `${event.clientX}px`;
-//   $elem.style.top = `${event.clientY}px`;
-});
-//마우스 업 이벤트 처리
-document.addEventListener("mouseup",(event)=>{
-  // console.log(event);
-  if(event.button === 2){ //오른쪽 버튼
-    console.log("오른쪽 버튼 업");
-  const $elem = document.querySelector(".coord");
-  $elem.style.left = `0`;
-  $elem.style.top = `0`;
-  console.log("mouseup");
-  }
+//   $elem.style.left = `0`;
+//   $elem.style.top = `0`;
+//   console.log("mouseup");
+//   }
 });
