70d66cd1a4b9f344f1f2c08805f750ff389fb96e
d-hy25 <rnjsehgus2525@gmail.com>
2025-02-20 11:12:11 +0900

    update

--- OsullocWebpublishing/script/menu.js
+++ OsullocWebpublishing/script/menu.js
@@ -1,45 +1,18 @@
-$(
-  function(){
-    $('.main-menu').hover(
-      //over
-      function(){
-        $('.sub-menu').stop().slideDown();
-        $('.menu-bg').stop().slideDown();
-      },
-      //out
-      function(){
-        $('.sub-menu').stop().slideUp();
-        $('.menu-bg').stop().slideUp();
-      }
-    );
-    // 탭 아이템 숨기기
-    $('.tab-item > ul').eq(1).hide();
-    // 공지사항 탭 선택
-    $('.tab-menu > li').click(
-      function(){
-        /* li 중에서 몇번째를 클릭했는지*/
-        /* 공지사항은 0 갤러리는 1*/
-        let idx = $(this).index();
-        $('.tab-menu > li').removeClass('tab-on');
-        $(this).addClass('tab-on');
-        // 탭 아이템 보이기
-        $('.tab-item > ul').hide().removeClass('tab-on');
-        $('.tab-item > ul').eq(idx).show().addClass('tab-on');
-      }
-    );
-        // 팝업창 띄우기
-        $('.notice > li').click(
-          function(){
-            // $('.popup').show();
-            $('.popup').fadeIn();
-          }
-        )
-    
-        $('.popup-card > button').click(
-          function(){
-            // $('.popup').hide();
-            $('.popup').fadeOut();
-          }
-        );
-  }
-)
+const main = document.querySelectorAll('nav > ul > .list:nth-of-type(1) > a:nth-of-type(1)');
+const mainTwo = document.querySelectorAll('nav > ul > .list:nth-of-type(4) > a:nth-of-type(4)');
+const subMenu = document.querySelectorAll('nav > ul > .list:nth-of-type(1) > .list-sub-menu:nth-of-type(1) > li');
+const subMenuTwo = document.querySelectorAll('nav > ul > .list:nth-of-type(4) > .list-sub-menu:nth-of-type(4) > li');
+
+  main.addEventListener("mouseenter", ()=> {
+    subMenu.classList.add('active');
+  });
+  main.addEventListener("mouseleave", ()=> {
+    subMenu.classList.remove('active');
+  });
+
+  mainTwo.addEventListener('mouseenter', ()=> {
+    subMenuTwo.classList.add('active')
+  });
+  mainTwo.addEventListener('mouseleave', ()=> {
+    subMenuTwo.classList.remove('active')
+  });
